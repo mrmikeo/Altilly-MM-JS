f0d81380d43ec0cf65305eb91dd6cbc1a1123fe7
Michael Osullivan <mike@altilly.com>
2020-11-17 04:25:40 +0000

    updates

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const altillyApi = require('nodeAltillyApi');
 const argv = require('yargs').argv
 const crypto = require('crypto');
 const WebSocket = require('ws');
+const { v4: uuidv4 } = require('uuid');
 
 const ws = new WebSocket('wss://wsapi.altilly.com:2096');
 
@@ -34,14 +35,9 @@ console.log(
         Stock Asset: ${opts.stock}
     `)
 
-/*
-const client = Binance({
-  apiKey: opts.apiKey,
-  apiSecret: opts.apiSecret
-});
-*/
-
 const restapi = new altillyApi.default(opts.apiKey, opts.apiSecret);
+
+restapi.cancelAllMarketOrders(opts.stock + opts.base);
 
 let lastPrice = 0;
 let is_initialised = false;
@@ -50,48 +46,72 @@ let rebalancing = false;
 ws.on('open', function open() {
 
   console.log('connected ws');
-  
-  doStuff();
+  doSubscribe();
   
 });
 
 ws.on('close', function close() {
+
   console.log('disconnected');
+  
+  restapi.cancelAllMarketOrders(opts.stock + opts.base);
+  
 });
 
-ws.on('message', function incoming(data) {
+setInterval(function() {
 
-  var data = JSON.parse(data);
-  
-  if (data.method == "ticker")
+	ws.ping();
+
+},5000);
+
+ws.on('pong', function() {
+
+	//console.log('pong');
+
+});
+
+ws.on('message', async function incoming(data) {
+
+  if (data !== undefined)
   {
-  
-  	lastPrice = parseFloat(data.last);
 
-    if (!is_initialised) {
-	  initialise();
-      is_initialised = true;
+    var data = JSON.parse(data);
+  
+    if (data.method == "ticker")
+    {
+  
+  	  lastPrice = parseFloat(data.params.last);
+
+      if (!is_initialised) {
+	    initialise();
+        is_initialised = true;
+      }
+  
     }
+    else if (data.method == "report")
+    {
+    
+      console.log(data);
   
-  }
-  else if (data.method == "report")
-  {
-  
-    if ((data.params.status === "partly filled" || data.params.status === "filled") && !rebalancing) { // Make sure we have async behaviour to avoid conflict
+      if ((data.params.status === "partly filled" || data.params.status === "filled") && !rebalancing) { // Make sure we have async behaviour to avoid conflict
         rebalancing = true;
         await cancel_all();
+        await sleep(2000);
         await recalculate_and_enter();
         rebalancing = false;
+      }
+  
     }
-  
+    
   }
-  
   
 });
 
-function doStuff()
+function doSubscribe()
 {
 
+  console.log('Do Subsribe');
+  
   ws.send(JSON.stringify({"method": "login","params": {"algo": "BASIC","pKey": opts.apiKey,"sKey": opts.apiSecret}}));
 
   // Stream the current price
@@ -104,104 +124,57 @@ function doStuff()
   // and enter new orders with a recalculated spread
 
   ws.send(JSON.stringify({"method":"subscribeReports","params":{}}));
+  
 }
 
-
-
-// Stream the current price
-// Save into a global variable
-/*
-let lastPrice = 0;
-let is_initialised = false;
-client.ws.trades(opts.stock + opts.base, trade => {
-  lastPrice = parseFloat(trade.price);
-  
-  // Initialise the market maker bot; This just runs 
-  // recalculate_and_enter() so that we have starter orders
-  if (!is_initialised) {
-    initialise();
-    is_initialised = true;
-  }
-});
-*/
-
-const orders = {
-    "buy": [],
-    "sell": [],
-};
-
-// Listen to our trades
-// If one of our buys gets filled, then cancel all orders 
-// and enter new orders with a recalculated spread
-
-/*
-let rebalancing = false;
-client.ws.user(async (msg) => {
-    if (msg.eventType === "executionReport") {
-        if ((msg.orderStatus === "partly filled" ||
-                msg.orderStatus === "filled") && !rebalancing) { // Make sure we have async behaviour to avoid conflict
-            rebalancing = true;
-            await cancel_all();
-            await recalculate_and_enter();
-            rebalancing = false;
-        }
-    }
-});
-*/
-
-
-
 async function cancel_all() {
-    await Object.keys(orders).forEach(order => {
-    
-    	client.cancelOrderById(order.uuid);
-    
-    /*
-        client.cancelOrder({
-            symbol: order.symbol,
-            orderId: order.orderId,
-        });
-    */
-    });
+    await restapi.cancelAllMarketOrders(opts.stock + opts.base);
 }
 
 // Enter a buy order with n% from account (y/2)% away from the last price
 // Enter a sell order with n% from accoutn (y/2)% away from the last price
-async function recalculate_and_enter() {
-    const account_info = await client.getTradingBalances();
-    
-    const base_balance = parseFloat(account_info.find(asset_obj => asset_obj.currency == opts.base).available);
-    const stock_balance = parseFloat(account_info.find(asset_obj => asset_obj.currency == opts.stock).available);
 
-    const quantity_stock = (stock_balance * opts.exposure).toFixed(5);
-    const quantity_base = (base_balance * opts.exposure).toFixed(5);
+async function recalculate_and_enter() {
+
+    const account_info = await restapi.getTradingBalances();
+
+	var balances = {};
+	for (let i = 0; i < account_info.length; i++)
+	{
+	
+		var thisitem = account_info[i];
+		
+		balances[thisitem.currency] = thisitem.available;
+
+	}
+
+    const base_balance = parseFloat(balances[opts.base]);
+    const stock_balance = parseFloat(balances[opts.stock]);
+
+    const sell_price = (lastPrice + (lastPrice * (opts.spread / 2))).toFixed(8);
+    const buy_price = (lastPrice - (lastPrice * (opts.spread / 2))).toFixed(8);
     
-    const sell_price = (lastPrice + (lastPrice * (opts.spread / 2))).toFixed(5);
-    const buy_price = (lastPrice - (lastPrice * (opts.spread / 2))).toFixed(5)
+    const quantity_stock = (stock_balance * opts.exposure).toFixed(3);
+    const quantity_base = ((base_balance * opts.exposure)/buy_price).toFixed(3);
 
     console.log(
         `
         Entering orders:
-            Buy amount (${opts.base}): ${quantity_base}
+            Buy amount (${opts.stock}): ${quantity_base}
             Buy price (${opts.base}): ${buy_price}
 
             Sell amount (${opts.stock}): ${quantity_stock}
-            Sell price (${opts.stock}): ${sell_price}
+            Sell price (${opts.base}): ${sell_price}
 
             Last Price: ${lastPrice} 
         `)
 
     for (const side of ["buy", "sell"]) {
-        orders[side].push(await client.createOrder(null, opts.stock + opts.base, side, type = 'limit', timeInForce = 'GTC', side === "buy" ? quantity_base :  quantity_stock, side === "buy" ? buy_price : sell_price));
-        
-        /*
-        {
-            symbol: opts.stock + opts.base,
-            side,
-            quantity: side === "buy" ? quantity_base :  quantity_stock,
-            price: side === "buy" ? buy_price : sell_price,
-        }));
-        */
+    
+    	var uuid = uuidv4();
+    	
+        await restapi.createOrder(uuid, opts.stock + opts.base, side, type = 'limit', timeInForce = 'GTC', side === "buy" ? quantity_base :  quantity_stock, side === "buy" ? buy_price : sell_price);
+
     }
 
 }
@@ -209,4 +182,10 @@ async function recalculate_and_enter() {
 function initialise() {
     console.log("Initialising...")
     recalculate_and_enter();
+}
+
+function sleep(ms) {
+  return new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
 }
